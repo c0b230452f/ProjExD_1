f7f13e46f10a0f73ffe713e486e1a36cd4bdcb5e
c0b23045 <c0b230452f@edu.teu.ac.jp>
2024-09-24 16:01:44 +0900

    ex7

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))  # ウィンドウサイズ
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像
+    bg_img_fl = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像
     kk_img = pg.transform.flip(kk_img, True, False)  # こうかとん画像を左右反転
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = tmr
-        x = -(x%800)
-        screen.blit(bg_img, [x, 0])
+        x = x%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_fl, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        # screen.blit(bg_img_fl, [-x+4800, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
